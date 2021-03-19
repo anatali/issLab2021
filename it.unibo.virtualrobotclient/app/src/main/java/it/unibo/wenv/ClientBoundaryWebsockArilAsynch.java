cad22e355688c09c9257ab1e6b5c025b47a04355
Antonio Natali <antonio.natali@unibo.it>
2021-03-19 11:17:28 +0100

    kotlintodo

--- it.unibo.virtualrobotclient/app/src/main/java/it/unibo/wenv/ClientBoundaryWebsockArilAsynch.java
+++ it.unibo.virtualrobotclient/app/src/main/java/it/unibo/wenv/ClientBoundaryWebsockArilAsynch.java
@@ -18,20 +18,20 @@ import it.unibo.supports.RobotApplicationStarter;
 @ArilRobotSpec
 public class ClientBoundaryWebsockArilAsynch {
     private RobotInputController controller;
-    private ActorRobotObserver actorObs = new ActorRobotObserver();
+    //private ActorRobotObserver actorObs = new ActorRobotObserver();
     //Constructor
     public ClientBoundaryWebsockArilAsynch(IssOperations rs){
         IssCommSupport rsComm = (IssCommSupport)rs;
         controller = new RobotInputController(rsComm, true, true );
         rsComm.registerObserver( controller );
-        rsComm.registerObserver( actorObs );
+        //rsComm.registerObserver( actorObs );
         System.out.println("ClientBoundaryWebsockBasicAsynch | CREATED with rsComm=" + rsComm);
     }
 
     public String doBoundary(){
         System.out.println("ClientBoundaryWebsockBasicAsynch | doBoundary " + controller  );
         String result = controller.doBoundary();
-        actorObs.close();
+        //actorObs.close();
         return result;
     }
 
