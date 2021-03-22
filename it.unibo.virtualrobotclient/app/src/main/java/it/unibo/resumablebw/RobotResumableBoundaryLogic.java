9a684f9702fefd428f93149b4a40b323783f8b1e
Antonio Natali <antonio.natali@unibo.it>
2021-03-22 22:58:16 +0100

    resumableBoundaryWalker

---
+++ it.unibo.virtualrobotclient/app/src/main/java/it/unibo/resumablebw/RobotResumableBoundaryLogic.java
@@ -0,0 +1,78 @@
+/*
+===============================================================
+RobotResumableBoundaryLogic.java
+implements the business logic  
+
+===============================================================
+*/
+package it.unibo.resumablebw;
+import it.unibo.interaction.MsgRobotUtil;
+import it.unibo.supports.IssCommSupport;
+
+
+public class RobotResumableBoundaryLogic {
+private IssCommSupport rs ;
+private int stepNum              = 1;
+private boolean boundaryWalkDone = false ;
+private boolean usearil          = false;
+private int moveInterval         = 1000;
+private RobotMovesInfo robotInfo;
+
+    public RobotResumableBoundaryLogic(IssCommSupport support, boolean usearil, boolean doMap){
+        rs           = support;
+        this.usearil = usearil;
+        robotInfo    = new RobotMovesInfo(doMap);
+     }
+
+    public void reset(){
+        stepNum          = 1;
+        boundaryWalkDone = false;
+        System.out.println("RobotBoundaryLogic | FINAL MAP:"  );
+        robotInfo.showRobotMovesRepresentation();
+        robotInfo.getMovesRepresentationAndClean();
+        robotInfo.showRobotMovesRepresentation();
+    }
+
+    public void doBoundaryGoon(){
+        rs.request( usearil ? MsgRobotUtil.wMsg : MsgRobotUtil.forwardMsg  );
+        delay(moveInterval ); //to reduce the robot move rate
+    }
+
+    public void updateMovesRep (String move ){
+        robotInfo.updateRobotMovesRepresentation(move);
+    }
+
+ //Business logic in RobotBoundaryLogic
+    public synchronized void boundaryStep( String move, boolean obstacle, boolean robotHalted ){
+         if (stepNum <= 4) {
+            if( move.equals("turnLeft") ){
+                updateMovesRep("l");
+                //robotInfo.showRobotMovesRepresentation();
+                if (stepNum == 4) {
+                    System.out.println("RobotBoundaryLogic | boundary ENDS"  );
+                    reset();
+                    return;
+                }
+                stepNum++;
+                if( ! robotHalted )  doBoundaryGoon();
+                return;
+            }
+            //the move is moveForward
+            if( obstacle && ! robotHalted){
+                rs.request( usearil ? MsgRobotUtil.lMsg : MsgRobotUtil.turnLeftMsg   );
+                delay(moveInterval ); //to reduce the robot move rate
+            }else if( ! obstacle ){
+                updateMovesRep("w");
+                if( ! robotHalted ) doBoundaryGoon();
+            }
+            robotInfo.showRobotMovesRepresentation();
+        }else{ //stepNum > 4
+            System.out.println("RobotBoundaryLogic | boundary ENDS"  );
+        }
+    }
+
+    protected void delay( int dt ){
+        try { Thread.sleep(dt); } catch (InterruptedException e) { e.printStackTrace(); }
+    }
+
+}
