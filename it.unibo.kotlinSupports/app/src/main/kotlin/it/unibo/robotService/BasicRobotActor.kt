43ab3689992b7521bf473358a7c17ec707146db3
Antonio Natali <antonio.natali@unibo.it>
2021-04-21 22:24:19 +0200

    irstActorRobot2021.html

--- it.unibo.kotlinSupports/app/src/main/kotlin/it/unibo/robotService/BasicRobotActor.kt
+++ it.unibo.kotlinSupports/app/src/main/kotlin/it/unibo/robotService/BasicRobotActor.kt
@@ -107,54 +107,5 @@ open class BasicRobotActor(name: String,  wenvAddr: String="wenv", scope: Corout
         updateObservers(answer) //a way to induce ActorContextTcpServer to send info to the sender
     }
 
-
-
-
-/*
-     }else{ //work
-         val msgJson = JSONObject( msg.msgContent.replace("@",",") )  //HORRIBLE trick
-         println( "$name | handleInput msgJson=" + msgJson)
-         if (msgJson.has("robotmove")) {
-
-         }else if (msgJson.has("robotmove")) { //"{\"robotmove\":\"turnLeft\", \"time\": 300}"
-             //The BasicRobotActor is engaged
-             if( ! engaged ) {
-                 engaged = true
-                 println("$name | send msgJson:$msgJson"   )
-                 support.forward(msgJson.toString())
-             }else{
-                 println( "$name | WARNING: alrady engaged"  )
-             }
-        }else if (msgJson.has(ApplMsgs.endMoveId)) {
-             val moveres  = msgJson.getString(ApplMsgs.endMoveId)
-             val movedone = msgJson.getString("move")
-             val payload  = "{\"${ApplMsgs.endMoveId}\":\"${moveres}\"@\"move\":\"$movedone\"}"
-             val answer   = MsgUtil.buildDispatch(name, ApplMsgs.endMoveId, payload, sender)
-             println("$name | send answer:" + answer)
-             if (engaged) {
-                 updateObservers(answer)
-                 engaged = false
-             }
-        }else if ( msgJson.has("sonarName")){
-            println("$name BasicRobotActor |  handleInput:$msgJson")
-            //val answerStr = "msg( MSGID, MSGTYPE, SENDER, RECEIVER, CONTENT, SEQNUM )"
-            val sonarName = msgJson.getString("sonarName")
-            val distance  = msgJson.getInt("distance").toString()
-            val sonarData = "${sonarName}_$distance"
-            val payload   = "{'sonarInfo':'${sonarData}'}"
-            val answer = MsgUtil.buildDispatch(name,"sonarInfo",payload, sender)
-             if( engaged ) this.updateObservers(answer)
-        }else if(msgJson.has("collision")) {
-            val payload   = "{'collision':'detected'}"
-            val answer = MsgUtil.buildDispatch(name,"collision", payload, sender)
-            println( "$name | send answer:" + answer)
-            if( engaged ) this.updateObservers(answer)
-        }
-
-    }//else work
-*/
-
-
-
 }
 
