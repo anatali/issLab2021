4e6222f7f52fb77181f73648305856c604b9defd
Antonio Natali <antonio.natali@unibo.it>
2021-05-20 12:06:09 +0200

    lez20May

--- it.unibo.qakradar2021/robotradar2020.pl
+++
@@ -1,6 +0,0 @@
-%====================================================================================
-% robotradar2020 description   
-%====================================================================================
-mqttBroker("localhost", "1883", "unibo/radar").
-context(ctxradargui, "localhost",  "TCP", "8038").
- qactor( radargui, ctxradargui, "it.unibo.radargui.Radargui").
--- it.unibo.qakradar2021/robotradar2021.pl
+++
@@ -1,6 +0,0 @@
-%====================================================================================
-% robotradar2021 description   
-%====================================================================================
-mqttBroker("localhost", "1883", "unibo/radar").
-context(ctxradargui, "localhost",  "TCP", "8038").
- qactor( radargui, ctxradargui, "it.unibo.radargui.Radargui").
