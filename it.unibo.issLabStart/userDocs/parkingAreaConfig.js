185e97253de57934bd562107bcd355a06cdc69ff
Antonio Natali <antonio.natali@unibo.it>
2021-06-03 12:45:06 +0200

    update

--- it.unibo.issLabStart/userDocs/parkingAreaConfig.js
+++ it.unibo.issLabStart/userDocs/parkingAreaConfig.js
@@ -3,57 +3,14 @@ const config = {
         size: { x: 31, y: 24                   }
     },
     player: {
-        //position: { x: 0.5, y: 0.5 },		//CENTER
-        position: { x: 0.10, y: 0.16 },		//INIT
-        //position: { x: 0.8, y: 0.85 },		//END
-        speed: 0.2
+         position: { x: 0.10, y: 0.16 },		//INIT
+         speed: 0.2
     },
     sonars: [
        ],
     movingObstacles: [
     ],
    staticObstacles: [
-   /*
-   
-        {
-            name: "plasticBox",
-            centerPosition: { x: 0.15, y: 1.0},
-            size: { x: 0.24, y: 0.07}
-        },
-
-		
-        {
-            name: "table",
-            centerPosition: { x: 0.60, y: 0.40},
-            size: { x: 0.16, y: 0.14      }
-		},
-
-        {
-            name: "bottle1",
-            centerPosition: { x: 0.55, y: 0.8 },
-            size: { x: 0.05, y: 0.05      }
-		},
-		
-        {
-            name: "bottle2",
-            centerPosition: { x: 0.18, y: 0.20},
-            size: { x: 0.05, y: 0.05      }
-		},
-
-
- 
-        {
-            name: "obs1",
-            centerPosition: { x: 0.05, y: 0.42 }, 
-            size: { x: 0.053, y: 0.041}
-        },
-
-        {
-            name: "obs2",
-            centerPosition: { x: 0.94, y: 0.42 }, 
-            size: { x: 0.053, y: 0.041}
-        },
-*/		 		 
         {
             name: "wallUp",
             centerPosition: { x: 0.44, y: 0.97},
