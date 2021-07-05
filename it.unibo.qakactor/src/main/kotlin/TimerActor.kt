7897f13aa04547eaeb996101d010903009783ec6
Antonio Natali <antonio.natali@unibo.it>
2021-07-05 19:25:41 +0200

    TimerActor revised

--- it.unibo.qakactor/src/main/kotlin/TimerActor.kt
+++ it.unibo.qakactor/src/main/kotlin/TimerActor.kt
@@ -3,31 +3,88 @@ package it.unibo.kactor
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class TimerActor(name: String, scope: CoroutineScope, val ctx : QakContext,
-                 val ev: String, val tout : Long) : ActorBasic(name,scope){
-var terminated = false;
+                 val ev: String, val tout : Long) : Thread() { //: ActorBasic(name,scope)
+    var terminated = false;
+    var starttime = 0L
+    var Duration  = 0L
+
+    lateinit var myactor : ActorBasic
     init{
         //println("TimerActor CREATED ${ev} time=$tout")
-        this.context = ctx
-        scope.launch{ autoMsg("start", "start") }
+        //this.context = ctx
+        //scope.launch{ autoMsg("start", "start") }
+
+        //Refactored July 2021, in order to avoid the usage of delay(tout)
+        val ownerName = ev.replace("local_tout_","").replace("_doStep","")
+        myactor       = QakContext.getActor(ownerName)!!
+        start()
     }
-@kotlinx.coroutines.ObsoleteCoroutinesApi
-@kotlinx.coroutines.ExperimentalCoroutinesApi
-   override suspend fun actorBody(msg : ApplMessage){
-        //println("TimerActor RECEIVES  ${msg} tout=$tout")
-        if( msg.msgId() == "start") {
-            delay(tout)
-            if( ! terminated ){
-                //println("TimerActor EMITS  ${ev} ")
-                emit(ev, ev)
+
+    override fun run(){
+        //println("TimerActor $name SLEEP tout=$tout ct=${System.currentTimeMillis()} ")
+        starttime     = System.currentTimeMillis()
+        Thread.sleep(tout)
+        val dd = System.currentTimeMillis() - starttime
+        //Runtime.getRuntime().exec("sudo ./sleepcrono")
+        //println("TimerThread  RESUMES after:${dd} ")
+        if( ! terminated ){
+            val msgEv = MsgUtil.buildEvent("timer",ev,ev)
+            runBlocking {
+                myactor.actor.send(msgEv)
             }
-            this.actor.close()
+            //MsgUtil.sendMsg("timer",ev,ev,"basicrobot", null)
+            println("TimerActor $name has EMITTED :${ev} ")
+        }else{
+            println("TimerActor $name ENDS without emitting since terminated after: $Duration")
         }
     }
 
+    /*
+@kotlinx.coroutines.ObsoleteCoroutinesApi
+@kotlinx.coroutines.ExperimentalCoroutinesApi
+   override suspend fun actorBody(msg : ApplMessage){
+        //sysUtil.trace
+    println("TimerActor using delay RECEIVES  ${msg} tout=$tout")
+        if( msg.msgId() == "start") {
+            /*
+            The thread is returned to the pool while the coroutine is waiting,
+            and when the waiting is done, the coroutine resumes on a free thread in the pool.
+             */
+            delay(tout)
+                // Thread.sleep(tout)
+            //delayUsingLinux()
+            if( ! terminated ){
+                emit(ev, ev)
+                println("TimerActor terminated=$terminated EMITTED :${ev} ")
+            }
+            this.actor.close()
+            //println("TimerActor $tout ENDS ")
+        }
+    }
+*/
+    fun delayUsingLinux(){
+        val p      = Runtime.getRuntime().exec("sudo ./cronoLinux")
+        val reader = BufferedReader(  InputStreamReader(p.getInputStream() ))
+        println(" startReaddddddddddddddddddddddddddddddd ")
+        //GlobalScope.launch{
+        while( true ){
+            var data = reader.readLine()
+            println("crono data = $data"   )
+            if( data == null ) break
+        }
+        //}
+
+    }
     fun endTimer(){
-        sysUtil.traceprintln("TimerActor $name TERMINATED ")
+        Duration = System.currentTimeMillis() - starttime
+        println("TimerActor $name TERMINATED duration=$Duration")
         terminated = true;
     }
+
+
 }
