5d73b76e3cce3ab6c2d759b980ce32cf0bc2c989
Antonio Natali <agoognat@gmail.com>
2021-07-12 16:51:51 +0200

    TestPlan0

--- it.unibo.qak21.basicrobot/test/it/unibo/qak21/basicrobot/CoapObserverForTesting.kt
+++ it.unibo.qak21.basicrobot/test/it/unibo/qak21/basicrobot/CoapObserverForTesting.kt
@@ -1,5 +1,34 @@
 package it.unibo.qak21.basicrobot
 
-object CoapObserverForTesting{
+import org.eclipse.californium.core.CoapClient
+import org.eclipse.californium.core.CoapHandler
+import org.eclipse.californium.core.CoapResponse
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.runBlocking
+
+object CoapObserverForTesting {
+   private val client = CoapClient()
+   private var handler : CoapHandler? = null
 	
+   fun addObserver(name: String, observed : String, channel : Channel<String>, expected:String ){
+	   val uriStr = "coap://localhost:8020/ctxbasicrobot/$observed"   
+	   println("$name | STARTTTTTTTTTTTTTTTT uriStr: $uriStr")
+       client.uri = uriStr
+       client.observe( object : CoapHandler {
+            override fun onLoad(response: CoapResponse) {
+				val content = response.responseText
+                println("$name | GET RESP-CODE= " + response.code + " content:" + content)
+				if( ! content.contains(expected) ) return
+                runBlocking{ channel.send(content) }
+ 			} 
+            override fun onError() {
+                println("$name | FAILED")
+            }
+        })		
+	}		 
+
+	fun removeObserver(  ){
+		client.delete()
+	}
+		
 }
--- it.unibo.qak21.basicrobot/test/it/unibo/qak21/basicrobot/TestPlan0.kt
+++ it.unibo.qak21.basicrobot/test/it/unibo/qak21/basicrobot/TestPlan0.kt
@@ -12,15 +12,19 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.channels.Channel
 import it.unibo.kactor.QakContext
 import org.junit.Before
+import it.unibo.kactor.ActorBasic
+import it.unibo.kactor.MsgUtil
+import org.junit.AfterClass
  
  
  
 
 class TestPlan0 {
-	
+		
 	companion object{
-		val channel       = Channel<String>()
-		var systemStarted = false
+		val channel               = Channel<String>()
+		var systemStarted         = false
+		var myactor : ActorBasic? = null
 
 		@JvmStatic
         @BeforeClass
@@ -32,15 +36,26 @@ class TestPlan0 {
 				it.unibo.ctxbasicrobot.main() //keep the control
 			}
 			GlobalScope.launch{
- 				while( QakContext.getActor("basicrobot") == null )		{
+				myactor=QakContext.getActor("basicrobot")
+ 				while(  myactor == null )		{
 					println("waiting for system startup ...")
 					delay(500)
+					myactor=QakContext.getActor("basicrobot")
 				}
-				channel.send("start")
+				
+				delay(3000)	//Give time to move
+				channel.send("starttesting")
 			}
-			//while( )
-		}
+		 
+		}//init
+		
+	@JvmStatic
+    @AfterClass
+	fun terminate() {
+		println("terminate")
 	}
+		
+	}//object
 	
 	@Before
 	fun checkSystemStarted()  {
@@ -53,17 +68,57 @@ class TestPlan0 {
 		}
  	}
 	
+	
   
-	@Test
-	fun test0(){
- 		println("test0 ........................")
-		assertEquals( "", "")
+	//@Test
+	fun testl(){
+ 		println("test1 ........................")
+		//Send a command and look at the result
+		var result  = ""
+		runBlocking{
+			val obsanswer = Channel<String>()
+			CoapObserverForTesting.addObserver("obsr", "basicrobot",obsanswer,"moveactivated")
+			MsgUtil.sendMsg("cmd","cmd(l)",myactor!!)
+			result = obsanswer.receive()
+			println("test1 RESULT=$result")
+			CoapObserverForTesting.removeObserver()
+		}
+		assertEquals( result, "moveactivated(l)")
 	}
 
-	@Test
-	fun test1(){
+	//@Test
+	fun testr(){
  		println("test1 ........................")
-		assertEquals( "", "")
+		//Send a command and look at the result
+		val cmd = MsgUtil.buildDispatch("tester", "cmd", "cmd(r)", "basicrobot")
+		var result  = ""
+		runBlocking{
+			val obsanswer = Channel<String>()
+			CoapObserverForTesting.addObserver("obsr", "basicrobot",obsanswer,"moveactivated")
+			MsgUtil.sendMsg(cmd, myactor!!)
+			result = obsanswer.receive()
+			println("test1 RESULT=$result")
+			CoapObserverForTesting.removeObserver()
+		}
+		assertEquals( result, "moveactivated(r)")
 	}
+	
+	@Test
+	fun testpNoobstacle(){
+ 		println("testp ........................")
+		//Send a command and look at the result
+		val request = MsgUtil.buildRequest("tester", "step", "step(500)", "basicrobot")
+		var result  = ""
+		runBlocking{
+			val obsanswer = Channel<String>()
+			CoapObserverForTesting.addObserver("obsp","basicrobot",obsanswer,"stepDone")
+			MsgUtil.sendMsg(request, myactor!!)
+			result = obsanswer.receive()
+			println("testp RESULT=$result")
+			CoapObserverForTesting.removeObserver()
+		}
+		assertEquals( result, "stepDone(500)")
+	}
+	
 		
 }
