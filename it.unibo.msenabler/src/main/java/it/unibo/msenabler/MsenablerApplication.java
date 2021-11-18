e76c050192783c40193929fd7c1883a5bbda70e7
Antonio Natali <agoognat@gmail.com>
2021-11-18 19:05:53 +0100

    msenabler

---
+++ it.unibo.msenabler/src/main/java/it/unibo/msenabler/MsenablerApplication.java
@@ -0,0 +1,13 @@
+package it.unibo.msenabler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsenablerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsenablerApplication.class, args);
+	}
+
+}
