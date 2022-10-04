5f1f028896c285d427c30bcff6ea41609b7b4c96
Antonio Natali <antonio.natali@unibo.it>
2022-10-04 15:21:39 +0200

    book example core

---
+++ wsdemoSTOMP/Eventuate/MyConsumer.java
@@ -0,0 +1,29 @@
+package io.eventuate.tram.examples.basic.messages;
+
+import io.eventuate.tram.messaging.common.Message;
+import io.eventuate.tram.messaging.consumer.MessageConsumer;
+import io.eventuate.tram.messaging.consumer.MessageHandler;
+import io.eventuate.tram.messaging.consumer.MessageSubscription;
+
+import java.util.Set;
+
+public class MyConsumer implements MessageConsumer {
+
+
+    @Override
+    public MessageSubscription subscribe(
+            String subscriberId, Set<String> channels, MessageHandler handler) {
+        System.out.println("MyConsumer subscribe " + subscriberId + " " + channels);
+        return null;
+    }
+
+    @Override
+    public String getId() {
+        return null;
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
---
+++ wsdemoSTOMP/Eventuate/MyProducer.java
@@ -0,0 +1,13 @@
+package io.eventuate.tram.examples.basic.messages;
+
+import io.eventuate.tram.messaging.common.Message;
+import io.eventuate.tram.messaging.producer.MessageProducer;
+
+public class MyProducer implements MessageProducer {
+
+    @Override
+    public void send(String destination, Message message) {
+        System.out.println("MyProducer send " + message.getPayload());
+
+    }
+}
